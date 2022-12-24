63a89f80d78044d93f0f927a792d2f4da19f76d5
Nithin <nithjoe1@gmail.com>
2022-12-24 12:03:27 -0500

    animated home page

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -6,11 +6,13 @@ const Home = () => {
         <>
         <div id="homepage">
             <h1 id="name">Nithin Joseph</h1>
-            <ul id="homelist">
-                <li>University of Maryland, College Park</li>
-                <li>Computer Science</li>
-                <li>Graduating May 2024</li>
-            </ul>
+            <div id="list">
+                <ul id="homelist">
+                    <li>University of Maryland, College Park</li>
+                    <li>Computer Science</li>
+                    <li>Graduating May 2024</li>
+                </ul>
+            </div>
         </div>
         </>
     );
